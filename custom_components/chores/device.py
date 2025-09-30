a45808386b9009cfbfa2b35e6e2b974a0fd85b62
Anthony Paul Willis <34542919+anthonypaulwillis@users.noreply.github.com>
2025-09-30 22:51:21 +0100

    Create device.py

---
+++ custom_components/chores/device.py
@@ -0,0 +1,15 @@
+class ChoreDevice:
+  def __init__(self, name: str):
+    self.name = name
+    self.points = 0
+    self.days = 0
+    self.last_done_by = ""
+    self.late_done_date = None
+    self.next_due_date = None
+    self.last_days_overdue = 0
+
+
+class ScoreDevice:
+  def __init__(self, name: str):
+    self.name = name
+    self.points = 0
