5f319ceb6b3f2ad5b096bca5a787369c31230a27
Anthony Paul Willis <34542919+anthonypaulwillis@users.noreply.github.com>
2025-09-30 14:41:55 +0100

    Update config_flow.py

--- custom_components/chores/config_flow.py
+++ custom_components/chores/config_flow.py
@@ -1,24 +1,22 @@
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.core import callback
-from .const import DOMAIN
+from .const import DOMAIN, CONF_TITLE
 
 class ChoresConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for the Chores integration."""
+    """Handle a config flow for Chores."""
 
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
 
     async def async_step_user(self, user_input=None):
         """Step when user adds the integration via UI."""
-
         if user_input is not None:
-            # The 'title' becomes the device name
-            return self.async_create_entry(title=user_input["title"], data=user_input)
+            return self.async_create_entry(
+                title=user_input[CONF_TITLE],
+                data=user_input
+            )
 
-        # Show a form with a single field: title
         data_schema = vol.Schema({
-            vol.Required("title"): str
+            vol.Required(CONF_TITLE): str
         })
-
         return self.async_show_form(step_id="user", data_schema=data_schema)
