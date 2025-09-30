b24a0ff702934512d78514c52ed0c52b4e92add9
Anthony Paul Willis <34542919+anthonypaulwillis@users.noreply.github.com>
2025-09-30 23:47:59 +0100

    Update const.py

--- custom_components/chores/const.py
+++ custom_components/chores/const.py
@@ -1,14 +1,14 @@
 DOMAIN = "chores"
 PLATFORMS = ["sensor", "number", "text", "datetime"]
 
-
 DEVICE_TYPE_CHORE = "chore"
 DEVICE_TYPE_SCORE = "score"
 
-
+# Attributes for Chore
 ATTR_POINTS = "points"
 ATTR_DAYS = "days"
 ATTR_LAST_DONE_BY = "last_done_by"
 ATTR_LATE_DONE_DATE = "late_done_date"
 ATTR_NEXT_DUE_DATE = "next_due_date"
 ATTR_LAST_DAYS_OVERDUE = "last_days_overdue"
+ATTR_STATUS = "status"
