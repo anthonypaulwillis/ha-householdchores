ed6f8f3ef7f124a8c2ad42edd4aad022739126f9
Anthony Paul Willis <34542919+anthonypaulwillis@users.noreply.github.com>
2025-09-30 23:28:40 +0100

    Update datetime.py

--- custom_components/chores/datetime.py
+++ custom_components/chores/datetime.py
@@ -7,26 +7,39 @@ from .const import DOMAIN, ATTR_LATE_DONE_DATE, ATTR_NEXT_DUE_DATE
 from .entity import ChoresEntity
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback):
+    """Set up Chore datetime entities (Next Due Date, Late Done Date)."""
     device = hass.data[DOMAIN][entry.entry_id]["device"]
     entities = []
 
-    # Only Chores have date fields
+    # Late Done Date
     if hasattr(device, "late_done_date"):
-        entities.append(ChoresDateTime(device, ATTR_LATE_DONE_DATE, "Late Done Date", entry.entry_id))
+        late_entity = ChoresDateTime(device, ATTR_LATE_DONE_DATE, "Late Done Date", entry.entry_id)
+        entities.append(late_entity)
+        # Register entity for service mapping
+        hass.data[DOMAIN][entry.entry_id]["device_entity_map"][late_entity.entity_id] = late_entity
+
+    # Next Due Date
     if hasattr(device, "next_due_date"):
-        entities.append(ChoresDateTime(device, ATTR_NEXT_DUE_DATE, "Next Due Date", entry.entry_id))
+        next_entity = ChoresDateTime(device, ATTR_NEXT_DUE_DATE, "Next Due Date", entry.entry_id)
+        entities.append(next_entity)
+        # Register entity for service mapping
+        hass.data[DOMAIN][entry.entry_id]["device_entity_map"][next_entity.entity_id] = next_entity
 
     async_add_entities(entities, True)
 
 
 class ChoresDateTime(ChoresEntity, DateTimeEntity):
+    """Datetime entity for Chores Next/Late dates."""
+
     def __init__(self, device, attr, name_suffix, entry_id):
         super().__init__(device, attr, name_suffix, entry_id)
 
     @property
     def native_value(self):
+        """Return current datetime value."""
         return getattr(self._device, self._attr)
 
     async def async_set_value(self, value):
+        """Set datetime value (used by service)."""
         setattr(self._device, self._attr, value)
         self.async_write_ha_state()
