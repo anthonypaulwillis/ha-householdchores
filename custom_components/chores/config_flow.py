1b37b659658d01871bdcd24d311101999b772369
Anthony Paul Willis <34542919+anthonypaulwillis@users.noreply.github.com>
2025-09-30 17:56:44 +0100

    Update config_flow.py

--- custom_components/chores/config_flow.py
+++ custom_components/chores/config_flow.py
@@ -1,22 +1,21 @@
 import voluptuous as vol
 from homeassistant import config_entries
-from .const import DOMAIN, CONF_TITLE
+from .const import DOMAIN, CONF_TITLE, CONF_TYPE, DEVICE_TYPE_CHORE, DEVICE_TYPE_SCORE
+
 
 class ChoresConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Config flow for Chores integration."""
+    """Handle a config flow for Chores."""
 
     VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
 
     async def async_step_user(self, user_input=None):
-        """Initial step for adding the integration through UI."""
+        errors = {}
         if user_input is not None:
-            return self.async_create_entry(
-                title=user_input[CONF_TITLE],
-                data=user_input
-            )
+            return self.async_create_entry(title=user_input[CONF_TITLE], data=user_input)
 
         schema = vol.Schema({
-            vol.Required(CONF_TITLE): str
+            vol.Required(CONF_TITLE): str,
+            vol.Required(CONF_TYPE, default=DEVICE_TYPE_CHORE): vol.In([DEVICE_TYPE_CHORE, DEVICE_TYPE_SCORE]),
         })
-        return self.async_show_form(step_id="user", data_schema=schema)
+
+        return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
