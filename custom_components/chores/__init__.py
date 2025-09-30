4155dd8d67b04e71022e375ff6c58f5fc51dd340
Anthony Paul Willis <34542919+anthonypaulwillis@users.noreply.github.com>
2025-09-30 23:02:13 +0100

    Update __init__.py

--- custom_components/chores/__init__.py
+++ custom_components/chores/__init__.py
@@ -2,18 +2,14 @@ from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.typing import ConfigType
 
-
 from .const import DOMAIN, PLATFORMS
 
-
 async def async_setup(hass: HomeAssistant, config: ConfigType):
-return True
-
+  return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-return True
-
+  await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+  return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+  return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
