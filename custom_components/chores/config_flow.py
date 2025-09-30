1cce05f905e3aa791146fafe8799db1897b53c76
Anthony Paul Willis <34542919+anthonypaulwillis@users.noreply.github.com>
2025-09-30 22:50:47 +0100

    Update config_flow.py

--- custom_components/chores/config_flow.py
+++ custom_components/chores/config_flow.py
@@ -1,21 +1,16 @@
 import voluptuous as vol
 from homeassistant import config_entries
-from .const import DOMAIN, CONF_TITLE, CONF_TYPE, DEVICE_TYPE_CHORE, DEVICE_TYPE_SCORE
+from .const import DOMAIN, DEVICE_TYPE_CHORE, DEVICE_TYPE_SCORE
+
+
+DEVICE_SCHEMA = vol.Schema({
+    vol.Required("name"): str,
+    vol.Required("device_type", default=DEVICE_TYPE_CHORE): vol.In([DEVICE_TYPE_CHORE, DEVICE_TYPE_SCORE]),
+})
 
 
 class ChoresConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for Chores."""
-
-    VERSION = 1
-
     async def async_step_user(self, user_input=None):
-        errors = {}
         if user_input is not None:
-            return self.async_create_entry(title=user_input[CONF_TITLE], data=user_input)
-
-        schema = vol.Schema({
-            vol.Required(CONF_TITLE): str,
-            vol.Required(CONF_TYPE, default=DEVICE_TYPE_CHORE): vol.In([DEVICE_TYPE_CHORE, DEVICE_TYPE_SCORE]),
-        })
-
-        return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
+            return self.async_create_entry(title=user_input["name"], data=user_input)
+        return self.async_show_form(step_id="user", data_schema=DEVICE_SCHEMA)
