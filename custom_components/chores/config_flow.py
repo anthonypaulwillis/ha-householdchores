7ec5e75f5075aee09072cfeee25d4d6ae2ae5b68
Anthony Paul Willis <34542919+anthonypaulwillis@users.noreply.github.com>
2025-10-01 22:19:03 +0100

    Update config_flow.py

--- custom_components/chores/config_flow.py
+++ custom_components/chores/config_flow.py
@@ -1,34 +1,20 @@
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.core import callback
-from .const import DOMAIN, DEVICE_TYPE_CHORE, DEVICE_TYPE_SCORE, ATTR_POINTS, ATTR_DAYS, ATTR_LAST_DONE_DATE, ATTR_NEXT_DUE_DATE
-from datetime import datetime, timedelta
+from .const import DOMAIN, CONF_NAME, CONF_DEVICE_TYPE, DEVICE_TYPE_CHORE, DEVICE_TYPE_SCORE
 
-DEVICE_TYPES = [DEVICE_TYPE_CHORE, DEVICE_TYPE_SCORE]
-
-@config_entries.HANDLERS.register(DOMAIN)
-class ChoresConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class HouseholdChoresConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        errors = {}
-
         if user_input is not None:
-            return self.async_create_entry(title=user_input["name"], data={
-                "name": user_input["name"],
-                "device_type": user_input["device_type"]
-            }, options={
-                ATTR_POINTS: user_input.get(ATTR_POINTS, 5),
-                ATTR_DAYS: user_input.get(ATTR_DAYS, 7),
-                ATTR_LAST_DONE_DATE: (datetime.utcnow()).isoformat(),
-                ATTR_NEXT_DUE_DATE: (datetime.utcnow() + timedelta(days=user_input.get(ATTR_DAYS,7))).isoformat()
-            })
+            return self.async_create_entry(title=user_input[CONF_NAME], data=user_input, options={})
 
-        data_schema = vol.Schema({
-            vol.Required("name"): str,
-            vol.Required("device_type", default=DEVICE_TYPE_CHORE): vol.In(DEVICE_TYPES),
-            vol.Optional(ATTR_POINTS, default=5): vol.All(int, vol.Range(min=0, max=20)),
-            vol.Optional(ATTR_DAYS, default=7): vol.All(int, vol.Range(min=0, max=365)),
+        schema = vol.Schema({
+            vol.Required(CONF_NAME): str,
+            vol.Required(CONF_DEVICE_TYPE, default=DEVICE_TYPE_CHORE): vol.In([DEVICE_TYPE_CHORE, DEVICE_TYPE_SCORE]),
         })
+        return self.async_show_form(step_id="user", data_schema=schema)
 
-        return self.async_show_form(step_id="user", data_schema=data_schema)
+    async def async_get_options_flow(self, entry):
+        from .options_flow import HouseholdChoresOptionsFlow
+        return HouseholdChoresOptionsFlow(entry)
