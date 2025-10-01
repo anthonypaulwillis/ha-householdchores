a90222eb26acd594dba5a0b57d78d38f097c1073
Anthony Paul Willis <34542919+anthonypaulwillis@users.noreply.github.com>
2025-10-01 18:47:56 +0100

    Update text.py

--- custom_components/chores/text.py
+++ custom_components/chores/text.py
@@ -1,31 +1,42 @@
 from homeassistant.components.text import TextEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-
-from .const import DOMAIN, ATTR_LAST_DONE_BY
+from .const import DOMAIN, "ATTR_LAST_DONE_BY"
 from .entity import ChoresEntity
 
 async def async_setup_entry(hass, entry: ConfigEntry, async_add_entities: AddEntitiesCallback):
-    if hass.data[DOMAIN][entry.entry_id].get("text_entities_added"):
-        return
-
     device = hass.data[DOMAIN][entry.entry_id]["device"]
     entities = []
 
     if hasattr(device, "last_done_by"):
-        last_by = ChoresText(device, ATTR_LAST_DONE_BY, "Last Done By", entry.entry_id)
-        entities.append(last_by)
-        hass.data[DOMAIN][entry.entry_id]["device_entity_map"][last_by.entity_id] = last_by
+        last_by_entity = ChoresTextEntity(device, "last_done_by", "Last Done By", entry.entry_id)
+        entities.append(last_by_entity)
+        hass.data[DOMAIN][entry.entry_id]["device_entity_map"][last_by_entity.entity_id] = last_by_entity
 
     async_add_entities(entities, True)
-    hass.data[DOMAIN][entry.entry_id]["text_entities_added"] = True
 
 
-class ChoresText(ChoresEntity, TextEntity):
+class ChoresTextEntity(ChoresEntity, TextEntity):
+    def __init__(self, device, attr_name, name, entry_id):
+        super().__init__(device, attr_name, name, entry_id)
+
     @property
     def native_value(self):
-        return getattr(self._device, self._attr)
+        return getattr(self._device, self._attr_name, "")
 
-    async def async_set_value(self, value: str):
-        setattr(self._device, self._attr, value)
+    async def async_set_value(self, value):
+        setattr(self._device, self._attr_name, value)
+        if hasattr(self._device, "status_sensor_entity") and self._device.status_sensor_entity:
+            self._device.status_sensor_entity.async_write_ha_state()
+        await self._persist_state()
         self.async_write_ha_state()
+
+    async def async_added_to_hass(self):
+        await super().async_added_to_hass()
+        if last_state := await self.async_get_last_state():
+            setattr(self._device, self._attr_name, last_state.state)
+
+    async def _persist_state(self):
+        entry = self.hass.data[DOMAIN][self._entry_id]["entry"]
+        from ..__init__ import persist_device_state
+        await persist_device_state(entry, self._device)
