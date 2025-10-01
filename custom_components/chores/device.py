0cff4fb608c09b79e6908c858bdd4ac104f7da92
Anthony Paul Willis <34542919+anthonypaulwillis@users.noreply.github.com>
2025-10-01 22:18:42 +0100

    Update device.py

--- custom_components/chores/device.py
+++ custom_components/chores/device.py
@@ -1,20 +1,18 @@
 from datetime import timedelta
 from homeassistant.util.dt import utcnow
 
-DEVICE_TYPE_CHORE = "chore"
-DEVICE_TYPE_SCORE = "score"
-
 class ChoreDevice:
-    def __init__(self, name: str, points=5, days=7, last_done_date=None, next_due_date=None):
+    def __init__(self, name: str, options: dict):
         self.name = name
-        self.device_type = DEVICE_TYPE_CHORE
         self.device_id = f"chores_{name.lower().replace(' ', '_')}"
-        self.points = points
-        self.days = days
-        self.last_done_date = last_done_date or utcnow()
-        self.next_due_date = next_due_date or self.last_done_date + timedelta(days=self.days)
+
+        now = utcnow()
+        self.days = options.get("days", 7)
+        self.points = options.get("points", 5)
+        self.last_done_by = options.get("last_done_by", "")
+        self.last_done_date = options.get("last_done_date", now)
+        self.next_due_date = options.get("next_due_date", now + timedelta(days=self.days))
         self.status = "unknown"
-        self.status_sensor_entity = None
 
     def update_status(self):
         now = utcnow()
@@ -29,10 +27,9 @@ class ChoreDevice:
         else:
             self.status = "unknown"
 
+
 class ScoreDevice:
-    def __init__(self, name: str, points=0):
+    def __init__(self, name: str, options: dict):
         self.name = name
-        self.device_type = DEVICE_TYPE_SCORE
-        self.device_id = f"scores_{name.lower().replace(' ', '_')}"
-        self.points = points
-        self.status_sensor_entity = None
+        self.device_id = f"score_{name.lower().replace(' ', '_')}"
+        self.points = options.get("points", 0)
