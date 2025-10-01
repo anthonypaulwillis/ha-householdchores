81a11052fcd13f45c129fe644c3ad2c81adb3e61
Anthony Paul Willis <34542919+anthonypaulwillis@users.noreply.github.com>
2025-10-01 21:56:09 +0100

    Update device.py

--- custom_components/chores/device.py
+++ custom_components/chores/device.py
@@ -5,15 +5,14 @@ DEVICE_TYPE_CHORE = "chore"
 DEVICE_TYPE_SCORE = "score"
 
 class ChoreDevice:
-    def __init__(self, name: str):
+    def __init__(self, name: str, points=5, days=7, last_done_date=None, next_due_date=None):
         self.name = name
         self.device_type = DEVICE_TYPE_CHORE
         self.device_id = f"chores_{name.lower().replace(' ', '_')}"
-        self.points = 5
-        self.days = 7
-        self.last_done_by = ""
-        self.last_done_date = utcnow()
-        self.next_due_date = utcnow() + timedelta(days=self.days)
+        self.points = points
+        self.days = days
+        self.last_done_date = last_done_date or utcnow()
+        self.next_due_date = next_due_date or self.last_done_date + timedelta(days=self.days)
         self.status = "unknown"
         self.status_sensor_entity = None
 
@@ -31,9 +30,9 @@ class ChoreDevice:
             self.status = "unknown"
 
 class ScoreDevice:
-    def __init__(self, name: str):
+    def __init__(self, name: str, points=0):
         self.name = name
         self.device_type = DEVICE_TYPE_SCORE
         self.device_id = f"scores_{name.lower().replace(' ', '_')}"
-        self.points = 0
+        self.points = points
         self.status_sensor_entity = None
